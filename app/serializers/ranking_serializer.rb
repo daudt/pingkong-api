868f0150e25b0b9aa1c5d28f1982a0eaa20637c0
Rafael Daudt <daudt@daudt.com>
2016-12-10 21:30:55 -0700

    polish ranking serializer

--- app/serializers/ranking_serializer.rb
+++ app/serializers/ranking_serializer.rb
@@ -1,3 +1,16 @@
 class RankingSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :rating, :created_at
+  attributes :user_id, :name, :email, :nickname, :rating, :created_at
+
+  def name
+    User.find(object.user_id).name
+  end
+
+  def email
+    User.find(object.user_id).email
+  end
+
+  def nickname
+    User.find(object.user_id).nickname
+  end
+
 end
