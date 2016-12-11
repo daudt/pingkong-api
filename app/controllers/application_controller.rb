298751a2d027a76547897135758376db238a31e7
Rafael Daudt <daudt@daudt.com>
2016-12-10 21:02:20 -0700

    fix missing user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name])
   end
 
 end
