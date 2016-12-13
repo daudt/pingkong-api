af63728ee98e986d7cfe50e5bffbc4078809af7a
Rafael Daudt <daudt@daudt.com>
2016-12-13 12:32:27 -0700

    create new ranking upon user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
+    @user.rankings.new(rating: user.rating)
     if @user.save
       render json: @user, status: :created, location: @user
     else
