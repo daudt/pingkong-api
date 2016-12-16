1cac008b8ff8901acd4c2ae1e295a82022805c63
Rafael Daudt <daudt@daudt.com>
2016-12-15 19:22:48 -0700

    fix rating not being set

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ApplicationRecord
   has_and_belongs_to_many :matches
   has_many :winners, dependent: :destroy
 
+  before_create :set_rating
+
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
 
   include DeviseTokenAuth::Concerns::User
+
+  def set_rating
+    self.rankings.new(rating: self.rating)
+  end
 
 end
