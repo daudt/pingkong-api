162830ced3c0474082fcd2a0887a04cfd294b49a
Rafael Daudt <daudt@daudt.com>
2016-12-18 08:54:21 -0700

    move api to /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :matches
-  resources :rankings, except: [:destroy, :create, :update]
-  mount_devise_token_auth_for 'User', at: 'auth'
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :matches
+    resources :rankings, except: [:destroy, :create, :update]
+    mount_devise_token_auth_for 'User', at: 'auth'
+    resources :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
