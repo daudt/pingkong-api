b854c72d1329c8d2930ae6c553879c93f7f9cfeb
Rafael Daudt <daudt@daudt.com>
2016-12-28 19:01:49 -0700

    enable emailing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -44,4 +44,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #Email settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'smtp.mailgun.org', :port => 587,
+                                         :user_name => ENV['MAILER_USERNAME_DEV'],
+                                         :password => ENV['MAILER_PASSWORD_DEV'] }
+
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,12 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'www.kingofpong.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'smtp.mailgun.org', :port => 587,
+                                         :user_name => ENV['MAILER_USERNAME_PROD'],
+                                         :password => ENV['MAILER_PASSWORD_PROD'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
