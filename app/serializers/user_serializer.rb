f0a31cb06db0d46d821bfce96d4cdeb438c3951c
ZenBlender <sbalay@gmail.com>
2017-01-01 12:14:22 -0700

    Add win/loss record to rankings.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :nickname, :image, :rating
+  attributes :id, :name, :email, :nickname, :image, :rating, :num_wins, :num_matches, :num_losses
+
+  def num_matches
+    object.matches.count
+  end
+
+  def num_wins
+    Winner.where(user_id: object.id).count
+  end
+
+  def num_losses
+    num_matches - num_wins
+  end
+
 end
