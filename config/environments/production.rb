a4cf0708b06dbcbd022f01e1dcfac618a207b6fc
Rafael Daudt <daudt@daudt.com>
2017-01-03 12:11:36 -0700

    Revert "enable prod debugging for now"
    This reverts commit c088d016c1fefe71e0a63c5aa0208316bb6e611b.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.log_level = ENV["LOG_LEVEL"].present? ? ENV["LOG_LEVEL"] : :debug
 end
