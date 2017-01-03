dfa8e6795241e3491f346c630c21ba8655008472
Rafael Daudt <daudt@daudt.com>
2017-01-02 23:06:32 -0700

    renamed user variable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
 
   before(:all) do
     @attributes = FactoryGirl.attributes_for(:user)
-    @player1 = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
   end
 
   it { should have_many :rankings}
@@ -18,23 +18,23 @@ describe User do
   it { should allow_value(@attributes[:image]).for(:image)}
 
   it 'should have an UUID' do
-    expect(@player1.uuid).to match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
+    expect(@user.uuid).to match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
   end
 
   it 'should have a initial rating of 1000' do
-    expect(@player1.rating).to equal(1000)
+    expect(@user.rating).to equal(1000)
   end
 
   it 'should have zero matches' do
-    expect(@player1.num_matches).to equal(0)
+    expect(@user.num_matches).to equal(0)
   end
 
   it 'should have zero wins' do
-    expect(@player1.num_wins).to equal(0)
+    expect(@user.num_wins).to equal(0)
   end
 
   it 'should have zero losses' do
-    expect(@player1.num_losses).to equal(0)
+    expect(@user.num_losses).to equal(0)
   end
 
   after(:all) do
