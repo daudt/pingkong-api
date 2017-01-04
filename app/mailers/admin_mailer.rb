8baadbf47b7d8d66597fd1f1afdf25992082e814
Rafael Daudt <daudt@daudt.com>
2017-01-03 17:42:37 -0700

    fix emailing

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,11 +1,11 @@
 class AdminMailer < ApplicationMailer
 
   def new_user_waiting_for_approval(user)
-    User.where(admin: true).each do |admin|
-      @user = user
-      @url = "#{api_users_url}/#{@user.uuid}/approve"
-      mail(to: admin.email, subject: 'New user awaiting approval')
-    end
+    recipients = User.where(admin: true)
+    emails = recipients.collect(&:email).join(',')
+    @user = user
+    @url = "#{api_users_url}/#{@user.uuid}/approve"
+    mail(to: emails, subject: 'New user awaiting approval')
   end
 
 end
