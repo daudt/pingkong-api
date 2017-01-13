fd9cbd269c02a2da92821bdecc2bd1ca2dde8ebc
Rafael Daudt <daudt@daudt.com>
2017-01-12 22:12:46 -0700

    users can only update themselves

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :matches, except: [:destroy, :update]
     resources :rankings, except: [:destroy, :create, :update]
     mount_devise_token_auth_for 'User', at: 'auth'
-    resources :users
+    resources :users, except: [:destroy, :create]
     get 'users/:uuid/approve', to: 'users#approve'
     get 'matches/:uuid/approve', to: 'matches#approve'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
