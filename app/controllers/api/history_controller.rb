9317cec8140a51dd3d31ea484df457b3d2a41fc3
ZenBlender <sbalay@gmail.com>
2017-01-16 08:50:04 -0700

    Add match ID to history.

--- app/controllers/api/history_controller.rb
+++ app/controllers/api/history_controller.rb
@@ -5,6 +5,7 @@ module Api
       matches = []
       Match.all.order(created_at: :desc).each do |match|
         matches << {
+          id:           match.id,
           winner_id:    match.winning_user.id,
           winner_name:  match.winning_user.preferred_name,
           loser_id:     match.losing_user.id,
